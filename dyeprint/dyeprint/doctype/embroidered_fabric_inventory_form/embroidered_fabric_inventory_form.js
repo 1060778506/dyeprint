471c2100c912ec29fbbd08a6b192457364c01d2a
fengmaode <1060778506@qq.com>
2026-06-04 11:19:28 +0000

    准备继续开发

---
+++ dyeprint/dyeprint/doctype/embroidered_fabric_inventory_form/embroidered_fabric_inventory_form.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Fengmaode and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Embroidered fabric inventory form", {
+// 	refresh(frm) {
+
+// 	},
+// });
